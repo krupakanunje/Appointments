d082b0d177fef27c4f2b6c4f5e55ffe394349293
krupakanunje <krupa.kanunje@gmail.com>
2022-03-01 15:29:16 +0530

    again

--- .venv/Appointment/Appointment/settings.py
+++ .venv/Appointment/Appointment/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +130,4 @@ STATIC_ROOT =os.path.join(BASE_DIR,'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())

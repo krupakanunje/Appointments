90d07fb8f585f0d7afc04d2e56be93f22ffaccf6
krupakanunje <krupa.kanunje@gmail.com>
2022-03-01 15:29:50 +0530

    ag

--- .venv/Appointment/Appointment/wsgi.py
+++ .venv/Appointment/Appointment/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import signal
 
+import sys
+import traceback
+
+import time
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Appointment.settings')
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+#from django.core.wsgi import get_wsgi_application
+
+
 
 application = get_wsgi_application()
